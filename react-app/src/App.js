a9a1c377275e5df1a83b7253a4de9f4e88421ec9
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2022-12-21 18:33:23 -0800

    format js files

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,25 +2,25 @@ import React, { useEffect } from 'react';
 import { getAllStudents } from './service/studentService';
 
 function App() {
-  const [students, setStudents] = React.useState([]);
+    const [students, setStudents] = React.useState([]);
 
-  useEffect(() => {
-    async function fetchStudents() {
-      let res = await getAllStudents();
-      setStudents(res);
+    useEffect(() => {
+        async function fetchStudents() {
+            let res = await getAllStudents();
+            setStudents(res);
+        }
+        fetchStudents();
+    }, []);
+
+    function logStudents() {
+        console.log(students);
     }
-    fetchStudents();
-  }, []);
 
-  function logStudents() {
-    console.log(students);
-  }
-
-  return (
-    <div className="App">
-      <button onClick={logStudents}>Get students</button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <button onClick={logStudents}>Get students</button>
+        </div>
+    );
 }
 
 export default App;
